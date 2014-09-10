1bf08278ff877115a90b582f5fb20913e62aafc9
Romain Paillard <romain.paillard@gmail.com>
2014-09-10 17:46:02 +0200

    define your own class completed

---
+++ Advanced/02-OOP/01-OO-Basics/01-Define-your-own-class/lib/pornstar.rb
@@ -0,0 +1,12 @@
+class Pornstar
+  def initialize (size, nickname,funny)
+    @size = size
+    @nickname = nickname
+    @funny = true
+  end
+  def funny?
+    return funny
+  end
+end
+
+rocco_siffredi = Pornstar.new(38,"Rocco","blond")
